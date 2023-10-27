5f6df8348cb5a29b10cd7db2ccc07d5ce33ad7ec
doem97 <doem1997@gmail.com>
2023-10-28 00:21:48 +0800

    dgx

--- exps/dgx/jigsaw_b_p56_336_in1k.sh
+++ exps/dgx/jigsaw_b_p56_336_in1k.sh
@@ -6,11 +6,12 @@ export THREADS=4
 python -m torch.distributed.launch \
     --nproc_per_node=$THREADS \
     --use_env \
+    --master_port 20000 \
     main_jigsaw.py \
     --model jigsaw_base_p56_336 \
     --input-size 336 \
-    --batch-size 768 \
-    --epochs 50 \
+    --batch-size 512 \
+    --epochs 10 \
     --sched cosine \
     --unscale-lr \
     --lr 1e-3 \
@@ -18,5 +19,7 @@ python -m torch.distributed.launch \
     --mask-ratio 0.0 \
     --data-path /workspace/study/imagenet/ILSVRC/Data/CLS-LOC \
     --data-set IMNET \
-    --output_dir ./outputs/jigsaw_base_p56_336_in1k
+    --output_dir ./outputs/jigsaw_base_p56_336_in1k_e10
+    # --resume "./outputs/jigsaw_base_p56_336_in1k/checkpoint_2.pth" \
+
     # --finetune /workspace/study/jigsawvit/jigdeit/data/jigsaw_base_results/best_checkpoint.pth \
