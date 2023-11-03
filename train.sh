3803f77d4aa71b7f0f659b16a6404c3b9e827b50
Donghao Huang <Donghao.Huang@mastercard.com>
2023-11-03 22:41:42 +0800

    updated train.sh

--- train.sh
+++ train.sh
@@ -6,22 +6,15 @@ pwd
 
 nvidia-smi
 
+# pip install -q timm
+
 # use stable learning rate 1e-3
-# export CUDA_VISIBLE_DEVICES="0,1,2,3"
-# export THREADS=4
-# export WANDB_MODE=disabled
-# export CUDA_VISIBLE_DEVICES="0,1,2,3"
-export THREADS=4
 export CONFIG_ID="jigsaw_base_p56_336_f101_shuffle_in1ke80fte100_e50_1e-3_1024"
 
 mkdir -p ./outputs/${CONFIG_ID}
 mkdir -p ./logs/${CONFIG_ID}
 
-python -m torch.distributed.launch \
-    --nproc_per_node=$THREADS \
-    --use_env \
-    --master_port 40000 \
-    main_jigsaw.py \
+torchrun main_jigsaw.py \
     --model jigsaw_base_patch56_336 \
     --input-size 336 \
     --batch-size 256 \
