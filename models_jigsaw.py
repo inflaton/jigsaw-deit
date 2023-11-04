ed976bfb2cd709c95b5c0f50a63fcaf1ec36563c
Donghao Huang <Donghao.Huang@mastercard.com>
2023-11-04 11:45:14 +0800

    try h1

--- models_jigsaw.py
+++ models_jigsaw.py
@@ -63,26 +63,26 @@ class JigsawVisionTransformer(VisionTransformer):
             # )  # ATTN: Original try: classifier head with three layers for small
 
             # For base model 768: h1
-            # self.cls_head = nn.Sequential(
-            #     # input should be 27648
-            #     nn.Linear(self.embed_dim * self.num_patches, 16384),  # 27648 -> 16384
-            #     nn.ReLU(),
-            #     nn.Linear(16384, 4096),
-            #     nn.ReLU(),
-            #     # nn.Linear(self.embed_dim * self.num_patches, self.num_classes),
-            #     nn.Linear(4096, self.num_classes),
-            #     nn.BatchNorm1d(self.num_classes),
-            # )
-
-            # For base model 768: h2
             self.cls_head = nn.Sequential(
                 # input should be 27648
-                nn.Linear(self.embed_dim * self.num_patches, 4096),  # 27648 -> 16384
+                nn.Linear(self.embed_dim * self.num_patches, 16384),  # 27648 -> 16384
+                nn.ReLU(),
+                nn.Linear(16384, 4096),
                 nn.ReLU(),
                 # nn.Linear(self.embed_dim * self.num_patches, self.num_classes),
                 nn.Linear(4096, self.num_classes),
                 nn.BatchNorm1d(self.num_classes),
             )
+
+            # For base model 768: h2
+            # self.cls_head = nn.Sequential(
+            #     # input should be 27648
+            #     nn.Linear(self.embed_dim * self.num_patches, 4096),  # 27648 -> 16384
+            #     nn.ReLU(),
+            #     # nn.Linear(self.embed_dim * self.num_patches, self.num_classes),
+            #     nn.Linear(4096, self.num_classes),
+            #     nn.BatchNorm1d(self.num_classes),
+            # )
 
             # self.cls_head = JigsawNet(
             #     num_classes=self.num_classes,
